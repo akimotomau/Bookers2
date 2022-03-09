5ff7d14a43bb1b115e94141b77d348af7b163598
EC2 Default User <ec2-user@ip-172-31-44-131.ec2.internal>
2022-03-09 12:43:47 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,10 @@ class Book < ApplicationRecord
   validates :body, presence: true, length: { maximum: 200 }
 
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only:[:index,:show,:edit, :update]
-  resources :books, only:[:index,:create,:show,:edit,:update,:destroy]
+  resources :books, only:[:index,:create,:show,:edit,:update,:destroy] do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create,:destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
